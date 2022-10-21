fba8a46a2e0baf6723a44a7f4c5b3fafb8d06dd2
Normando Ali Zubia Hernández <nzubia@blacklabs.mx>
2022-10-20 18:48:46 -0600

    Se agrega consulta para obtención de todos los usuarios

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -34,6 +34,25 @@ async function createUser(req, res) {
 	}
 }
 
+async function findUsers(req, res) {
+
+	try {
+		const users = await User.find({});
+
+		res.status(200).json({
+			message: "All users in DB:",
+			obj: users
+		})
+	}catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something happend when storing user",
+			obj: null
+		})
+	}
+}
+
 module.exports = {
-	createUser
+	createUser,
+	findUsers
 }
--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,6 @@ router.get('/', function(req, res, next) {
 
 router.post('/create', userController.createUser);
 
+router.get('/all', userController.findUsers);
+
 module.exports = router;
